22bf4e60645bbd86572c2fa1e1b4a8bd9f714cf5
Raul P <55366395+radape-Hub@users.noreply.github.com>
2022-10-29 14:37:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 
-    datosOptenidos = requests.get('https://api.dailymotion.com/videos?channel=sport&limit=10')
+    datosOptenidos = requests.get('https://api.dailymotion.com/videos?channel=sport&limit=20')
     datosformatoJson= datosOptenidos.json()
     
 
