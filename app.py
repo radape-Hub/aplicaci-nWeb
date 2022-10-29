d55d4463e771aaf1b70fe6dfe714a9f2dde282d8
radape-Hub <geeksoyla@gmail.com>
2022-10-29 11:44:43 -0500

    Se agrego la Api y se modifico el Index

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
+import requests
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+
+    datosOptenidos = requests.get('https://api.dailymotion.com/videos?channel=sport&limit=10')
+    datosformatoJson= datosOptenidos.json()
+    print(datosformatoJson)
+
+
+    return render_template('index.html', datos=datosformatoJson['list'])
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
